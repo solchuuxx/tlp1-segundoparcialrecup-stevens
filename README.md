16903814b7e7bd2f7324776cc1e214f01b6c1228
solchuuxx <natalistevens1@hotmail.com>
2023-07-05 14:32:55 -0300

    Se completó los datos faltantes en el archivo app.js, también se configuró el archivo .env

---
+++ README.md
@@ -0,0 +1,18 @@
+#Examen Parcial Recuperatorio
+Instalacion:
+```bash
+npm install
+```
+Configurar variables de entorno:
+PORT=
+DB_NAME=
+DB_PASSWORD=
+DB_HOST
+DB_PORT=
+DB_DIALECT=
+
+
+Ejecución en desarrollo:
+```bash
+npm run dev
+```
--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 // Imports
+require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
-
+const port = process.env.PORT || 5000;
 const path = require('path');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
 const app = express();
 
 // Middlewares
 // TODO: Implementar middlewares
-
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-app.use('/api', require('./routes/reserva.routes'));
+app.use(require('./routes/reserva.routes'));
 
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
+app.use((req, res, next) => {
+	return res.status(404).render('404');
+});
+
 
 // Starting the server
-app.listen(45635, () => console.log('Server on port xxxx'));
+app.listen(port, () => console.log('Server on http://localhost:${port}'));
